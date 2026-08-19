24142f4ee79bcca58dc2adbd27873f2c90183880
iNWEKOBONG <essiengodspower447@gmail.com>
2026-08-19 12:09:49 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/USSD-UI/', // 👈 ADD THIS EXACT LINE HERE
   plugins: [react(), tailwindcss()],
   test: {
     environment: "node",
